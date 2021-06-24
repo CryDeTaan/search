c87d11824a5544420146492b1bf40feb4328e57a
CryDeTaan <CryDeTaan@users.noreply.github.com>
2021-06-24 06:39:55 +0100

    🐛 Action should be on the form not the input.

--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -9,8 +9,8 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
-                    <form action="#" method="get" class="space-y-2 mb-6">
-                        <x-input action="/search" id="search" name="search" type="search" placeholder="Search..." class="block w-full" />
+                    <form action="/search" method="get" class="space-y-2 mb-6">
+                        <x-input id="search" name="search" type="search" placeholder="Search..." class="block w-full" />
                         <x-button>Search</x-button>
                     </form>
 
