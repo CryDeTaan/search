c5ee1347f64745a026fe86fdc175dd0595115924
CryDeTaan <CryDeTaan@users.noreply.github.com>
2021-06-24 06:12:13 +0100

    🚧 Added basic search form.

--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -9,7 +9,10 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
-                    Search
+                    <form action="#" method="get" class="space-y-2">
+                        <x-input action="/search" id="search" name="search" type="search" placeholder="Search..." class="block w-full" />
+                        <x-button>Search</x-button>
+                    </form>
                 </div>
             </div>
         </div>
